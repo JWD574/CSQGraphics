b9ec170b5bcc7a55aedb72b27318850e2d216d4d
rangerboy574 <jdeweese574@gmail.com>
2023-01-07 14:21:33 -0600

    Initial Commit

---
+++ CSQGraphics/CSQGraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSQGraphicsApp.swift
+//  CSQGraphics
+//
+//  Created by Joseph  DeWeese on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CSQGraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
